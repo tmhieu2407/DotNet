182123f75171d6727141385c0b48a9069b6801ab
tmhieu2407 <htran31000@gmail.com>
2024-09-19 12:55:07 +0700

    project

---
+++ Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Models1;
+using MVC.Areas.Admin.Code;
+using MVC.Areas.Admin.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public ActionResult Index(Login_NetModels models)
+        {
+            var result = new LoginModels().Login(models.Username, models.PasswordHash);
+            if(result && ModelState.IsValid)
+            {
+                SessionHelper.SetSession(new UserSession() {Username = models.Username });
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tài khoản mật khẩu không đúng!");
+            }
+            return View(models);
+        }
+    }
+}
---
+++ Areas/Admin/Data/Login_NetModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Areas.Admin.Data
+{
+    public class Login_NetModels
+    {
+        [Required] 
+        public string Username { set; get; }
+        public string PasswordHash { set; get; }
+        public bool Remember { set; get; }
+
+    }
+}
